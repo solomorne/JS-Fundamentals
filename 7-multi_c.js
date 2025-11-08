b0fdf0af7020f4c4dc36789e4ed02f618a49fdc9
solomon amaechi <solomornejohn@gmail.com>
2025-11-08 10:57:47 +0100

    add multiple print script

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,12 @@
-const x = parseInt(process.argv[2]);
+const arg = process.argv[2];
+const n = parseInt(arg, 10);
 
-if (isNaN(x) || x < 0) {
+if (isNaN(n)) {
   console.log("Missing number of occurrences");
 } else {
-  let output = "";
-  for (let i = 0; i < x; i++) {
-    output += "C is fun\n";
+  let out = "";
+  for (let i = 0; i < n; i++) {
+    out += "C is fun\n";
   }
-  console.log(output.trim());
+  if (out) console.log(out.trimEnd());
 }
